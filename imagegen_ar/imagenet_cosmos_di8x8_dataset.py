88bcfd68096ea8483915486b67c29d93445cdd95
Prashant Shishodia <prashant.cse.iitkgp@gmail.com>
2025-05-06 22:03:49 +0000

    append start token & only use 128x128 random tiles instead of 256x256 for speedup

--- imagegen_ar/imagenet_cosmos_di8x8_dataset.py
+++ imagegen_ar/imagenet_cosmos_di8x8_dataset.py
@@ -5,15 +5,19 @@ from typing import Any, Dict, List, Mapping, Optional
 from datasets import load_dataset
 from torch.utils.data import Dataset
 from torchtune.datasets._packed import PackedDataset
+import random
 
 
 class ImagenetCosmosDataset(Dataset):
     def __init__(
         self,
         column: str = "text",
+        im_start:  int = None,
+        im_end: int = None
     ) -> None:
         self._column = column # 'cosmos_di8x8_tokens'
         self._data = load_dataset("pshishodia/imagenet-1k-256", split='train', columns=[self._column], num_proc=64)
+        self._im_start, self._im_end = im_start, im_end
         
     def __len__(self):
         return len(self._data)
@@ -24,10 +28,13 @@ class ImagenetCosmosDataset(Dataset):
 
     def _prepare_sample(self, sample: Mapping[str, Any]) -> Dict[str, List[int]]:
         # expecting a 2d list. 
-        twod_tokens = sample[self._column]
+        twod_tokens = sample[self._column] # 32 x 32
+        
+        row_start, col_start = random.randint(0, 16), random.randint(0, 16)
+        twod_tokens = [row[col_start:col_start + 16] for row in twod_tokens[row_start:row_start + 16]] # 32 x 32
         
         # torchtune.datasets.TextCompletionDataset: No need to offset labels by 1 - happens in the recipe
-        tokens = [token for row in twod_tokens for token in row]
+        tokens = [self._im_start] + [token for row in twod_tokens for token in row]
         return {"tokens": tokens, "labels" : tokens.copy()}
 
 
@@ -35,10 +42,14 @@ def imagenet_cosmos_dataset(
     column: str = "text",
     packed: bool = False,
     split_across_pack: bool = True,
-    max_seq_len: Optional[int] = None
+    max_seq_len: Optional[int] = None,
+    im_start: int = None,
+    im_end: int = None,
 ):
     ds = ImagenetCosmosDataset(
         column=column,
+        im_start=im_start,
+        im_end=im_end
     )
     if packed:
         if max_seq_len is None:
